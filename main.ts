16aa563f30c00c675bf4efbd3cf6372562f039db
doradonatalyrafaela <70153479+doradonatalyrafaela@users.noreply.github.com>
2020-08-24 19:32:47 -0400

    Agregue mensajes

--- main.ts
+++ main.ts
@@ -79,11 +79,11 @@ controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
         `)
 })
 let Heroina: Sprite = null
-game.splash("Habia una vez dos mejores amigas que se querian mucho")
-game.splash("Un día unos robots malos los atacaron")
-game.splash("Y separaron a las amigas un dia una de ellas quizo visitar a la otra")
-game.splash("Pero los robots la encontraron y la llevaron muy lejos")
-game.splash("Tu misión es ayudar a tu amiga salvandola")
+game.showLongText("Habia una vez dos mejores amigas que se querian mucho", DialogLayout.Top)
+game.showLongText("Un día unos robots malos los atacaron", DialogLayout.Top)
+game.showLongText("Y separaron a las amigas un dia una de ellas quizo visitar a la otra", DialogLayout.Top)
+game.showLongText("Pero los robots la encontraron y la llevaron muy lejos", DialogLayout.Top)
+game.showLongText("Tu misión es ayudar a tu amiga salvandola", DialogLayout.Top)
 scene.setBackgroundColor(9)
 Heroina = sprites.create(img`
     . . . . . . . . . . . . . . . . 
