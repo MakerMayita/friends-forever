ce0a0bdb97216efcd9bacb9d374dff21c19fc463
MakerNaomi <70153474+MakerNaomi@users.noreply.github.com>
2020-08-25 12:15:00 -0400

    cambie el sprite

--- main.ts
+++ main.ts
@@ -93,21 +93,21 @@ Heroina = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     `, SpriteKind.Player)
 let mySprite = sprites.create(img`
-    . . . . . . . . . . b 5 b . . . 
-    . . . . . . . . . b 5 b . . . . 
-    . . . . . . b b b b b b . . . . 
-    . . . . . b b 5 5 5 5 5 b . . . 
-    . . . . b b 5 d 1 f 5 5 d f . . 
-    . . . . b 5 5 1 f f 5 d 4 c . . 
-    . . . . b 5 5 d f b d d 4 4 . . 
-    . b b b d 5 5 5 5 5 4 4 4 4 4 b 
-    b d d d b b d 5 5 4 4 4 4 4 b . 
-    b b d 5 5 5 b 5 5 5 5 5 5 b . . 
-    c d c 5 5 5 5 d 5 5 5 5 5 5 b . 
-    c b d c d 5 5 b 5 5 5 5 5 5 b . 
-    . c d d c c b d 5 5 5 5 5 d b . 
-    . . c b d d d d d 5 5 5 b b . . 
-    . . . c c c c c c c c b b . . . 
+    . f f . . . . . . . . . f f . . 
+    . f 4 f . . . . . . . f 4 f . . 
+    . f 3 4 f . . . . . f 4 3 f . . 
+    . . f 3 4 f f f f f 4 3 f . . . 
+    . . f 3 3 4 4 4 4 4 3 3 f . . . 
+    . . . f 4 4 4 4 4 4 4 f . . . . 
+    . . . f 4 f 4 4 4 f 4 f . . . . 
+    . . . f 3 4 4 1 4 4 3 f . . . . 
+    . . . . f 4 1 f 1 4 f . . . . . 
+    . . . . . f f f f f . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     `, SpriteKind.Player)
 controller.moveSprite(Heroina, 100, 0)
