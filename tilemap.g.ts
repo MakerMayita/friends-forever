a255dcd1611c695185924c5ab2cae69a33d9b309
MakerNaomi <70153474+MakerNaomi@users.noreply.github.com>
2020-08-30 15:48:19 -0400

    hice el mapa nivel 2

--- tilemap.g.ts
+++ tilemap.g.ts
@@ -18,6 +18,12 @@ namespace myTiles {
     export const tile7 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
     export const tile8 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile9 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile10 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile11 = image.ofBuffer(hex``);
 
 }
 // Auto-generated code. Do not edit.
